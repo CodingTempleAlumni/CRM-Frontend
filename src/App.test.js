c813bbb40a95746193b91b3ba2fef5b7eeb1f767
nwelter1 <nwelter1@gmail.com>
2022-09-26 13:48:03 -0500

    initial testing module

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
+// TODO: Get some more initial barebones tests here! SHould have tests for every component/module!
 it('renders welcome message', () => {
   render(<App />);
   expect(screen.getByText('Welcome to the CTA Open Source Project!')).toBeInTheDocument();
